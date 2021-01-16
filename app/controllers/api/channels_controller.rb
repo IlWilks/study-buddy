8abfb8161db378787e01a9aa0c0df530e2a87464
Joshua Parco <jacparco@gmail.com>
2021-01-16 10:47:35 -0700

    channels controller

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,22 +1,43 @@
 class Api::ChannelsController < ApplicationController
+  before_action :set_channel, only: [:show, :update, :destroy]
+
   def index
+    render json: Channel.all
   end
 
   def show
-  end
-
-  def new
+    render json: @channel
   end
 
   def create
-  end
-
-  def destroy
-  end
-
-  def edit
+    channel = Channel.new(channel_params)
+    if channel.save
+      render json: channel
+    else
+      render json: { errors: channel.errors }, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @channel.update(channel_params)
+      render json: @channel
+    else
+      render json: @channel.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @channel.destroy
+    render json: { message: 'Channel deleted' }
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find(params[:id])
+  end
+
+  def channel_params
+    params.require(:channel).permit(:subject, :public, :description)
   end
 end
