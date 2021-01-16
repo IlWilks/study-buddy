265cdef4ea994167cb614612f0b43581266631d7
dani7beth <danielledona28@gmail.com>
2021-01-16 10:39:12 -0700

    filled out group controller

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,22 +1,39 @@
 class Api::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_exercises, only: [:update, :destroy, :show]
+
   def index
+    render json: @current_user.groups.all
   end
 
   def show
-  end
-
-  def new
+    render json: @group
   end
 
   def create
+    group = @current_user.groups.new(group_params)
+    if group.save
+      render json: group
+    else
+      render json: {errors: group.errors}, status: 422
+    end
   end
 
   def destroy
-  end
-
-  def edit
+    @group.destroy
+    render json: @group
   end
 
   def update
+    @group.update(group_params)
+    render json: @group
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+  def set_group
+    @group = @current_user.groups.find(params[:id])
   end
 end
